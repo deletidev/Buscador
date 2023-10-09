c31ee7a38f18515b376c0f537fa0199d56d9927a
LeticiadelaOsa <deletidev@gmail.com>
2023-10-09 20:10:29 +0200

    theme

--- src/scenes/rick-detail.scene.tsx
+++ src/scenes/rick-detail.scene.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import { path } from '@/core';
+import { path, rickTheme, useMyThemeContext } from '@/core';
 import { RickLayout } from '@/layout';
 import { RickDetailContainer } from '@/pods';
 
 export const RickDetailScene: React.FC = () => {
+  const { setTheme } = useMyThemeContext();
+  React.useEffect(() => {
+    setTheme(rickTheme);
+  }, []);
+
   return (
     <RickLayout rute={path.SEARCHRICK}>
       <RickDetailContainer></RickDetailContainer>
