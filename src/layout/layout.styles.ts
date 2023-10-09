85b5dc9fdd445b3b499fdd141e834b8cd9b95b82
LeticiadelaOsa <deletidev@gmail.com>
2023-10-09 18:43:15 +0200

    cambio tipografia

--- src/layout/layout.styles.ts
+++ src/layout/layout.styles.ts
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 
 import backgroundImg from '@/assets/fondoRyMpx.jpg';
-import { rickTheme } from '@/core';
 
 const urlBackgroundImage = `url(${backgroundImg})`;
 
@@ -30,7 +29,6 @@ export const headerCss = {
 };
 
 export const rick = css`
-  font-family: ${rickTheme.typography.fontFamily};
   background-image: ${urlBackgroundImage};
   background-repeat: no-repeat;
   background-position: bottom center;
